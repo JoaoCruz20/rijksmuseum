fffdb6ded38a76779c98e52349de9c7313fc30cc
JoaoCruzWoW <104000000+JoaoCruzWoW@users.noreply.github.com>
2023-01-24 15:15:36 +0000

    router changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,21 @@ import Home from "../src/pages/home";
 import Profile from "../src/pages/profile";
 import About from "../src/pages/about";
 import SearchPage from "../src/pages/search";
+import { Switch } from 'react-router';
 
 function App() {
   return (
     <Router> 
+      <Switch>
       <Routes>
-        <Route caseSensitive={false} path="/" element={<Home />} />
+        <Route caseSensitive={false} path="/"  element={<Home />}>
+          <Home />
+        </Route>
         <Route caseSensitive={false} path="/profile" element={<Profile />} />
         <Route caseSensitive={false} path="/search" element={<SearchPage />} />
         <Route caseSensitive={false} path="/about" element={<About />} />
-      </Routes>          
+      </Routes>  
+      </Switch>        
     </Router>
   );
 }
